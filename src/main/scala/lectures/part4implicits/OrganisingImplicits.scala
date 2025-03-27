fdafae8fe640ce4812e49aae4c6d85983eb72405
Omiros Trypatsas <omiros.trypatsas@mercator.group>
2025-03-27 13:06:56 +0000

    Organising implicits done

--- src/main/scala/lectures/part4implicits/OrganisingImplicits.scala
+++ src/main/scala/lectures/part4implicits/OrganisingImplicits.scala
@@ -26,8 +26,55 @@ object OrganisingImplicits extends App {
     Person("John", 66)
   )
 
-  implicit def reverseAlphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a,b) => a.name.compareTo(b.name) < 0)
+//  object Person {
+//    implicit def alphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0)
+//  }
 
+//  implicit def ageOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.age < b.age)
+//  println(persons.sorted)
+
+  /*
+    Implicit scope
+    - normal scope = LOCAL SCOPE
+    - imported scope
+    - companion objects of all types involved in the method signature
+      - List
+      - Ordering
+      - all the types involved
+   */
+
+  // def sorted[B >: A](implicit ord: Ordering[B]): List[B]
+
+  object AlphabeticNameOrdering {
+    implicit def alphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0)
+  }
+
+  object AgeOrdering {
+    implicit def ageOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.age < b.age)
+  }
+
+  import AgeOrdering.ageOrdering
   println(persons.sorted)
+
+  /*
+    Exercise.
+    - totalPrice = most used (50%)
+    - by unit count (25%)
+    - by unit price (25%)
+   */
+
+  case class Purchase(nUnits: Int, unitPrice: Double)
+
+  object Purchase {
+    implicit val totalPriceOrdering: Ordering[Purchase] = Ordering.fromLessThan((a,b) => a.unitPrice * a.nUnits < b.unitPrice * b.nUnits)
+  }
+
+  object UnitCountOrdering {
+    implicit val unitCountOrdering: Ordering[Purchase] = Ordering.fromLessThan(_.nUnits < _.nUnits)
+  }
+
+  object UnitPriceOrdering {
+    implicit val unitPriceOrdering: Ordering[Purchase] = Ordering.fromLessThan(_.unitPrice < _.unitPrice)
+  }
 
 }

488eec84e51d0c33bd342eb27cb3f77f29d285e4
Omiros Trypatsas <omiros.trypatsas@mercator.group>
2025-06-12 18:55:30 +0100

    Section 5 - Given Instances and Using Clauses

--- src/main/scala/lectures/part4implicits/Givens.scala
+++ src/main/scala/lectures/part4implicits/Givens.scala
@@ -30,11 +30,55 @@ object Givens extends App {
   // import GivenWith._ // in Scala 3, this import does NOT import givens as well
   // import GivenWith.given // imports all givens
 
-  // implicit arguments?
+  // implicit arguments <=> using clauses
   def extremes[A](list: List[A])(implicit ordering: Ordering[A]): (A, A) = {
     val sortedList = list.sorted
     (sortedList.head, sortedList.last)
   }
 
+  // Scala 3 equivalent
+//  def extremes_v2[A](list: List[A])(using ordering: Ordering[A]): (A, A) = {
+//    val sortedList = list.sorted // ordering
+//    (sortedList.head, sortedList.last)
+//  }
+
+  // implicit def (synthesize new implicit values)
+  trait Combinator[A] { // semigroup
+    def combine(x: A, y: A): A
+  }
+
+  implicit def listOrdering[A](implicit simpleOrdering: Ordering[A], combinator: Combinator[A]): Ordering[List[A]] = {
+    new Ordering[List[A]] {
+      override def compare(x: List[A], y: List[A]) = {
+        val sumX = x.reduce(combinator.combine)
+        val sumY = y.reduce(combinator.combine)
+        simpleOrdering.compare(sumX, sumY)
+      }
+    }
+  }
+
+  // equivalent in Scala 3 with givens
+//  given listOrdering_v2[A](using simpleOrdering: ordering[A]), combinator: Combinator[A]): Ordering[List[A]]with {
+//    override def compare(x: List[A], y: List[A]) = {
+//      val sumX = x.reduce(combinator.combine)
+//      val sumY = y.reduce(combinator.combine)
+//      simpleOrdering.compare(sumX, sumY)
+//    }
+//  }
+
   println(anOrderedList)
+
+  // implicit conversions (abused in Scala 2)
+  case class Person(name: String) {
+    def greet(): String = s"Hi, my name is: $name."
+  }
+
+  implicit def string2Person(string: String): Person = Person(string)
+  val omirosGreet = "Omiros".greet() // string2Person("Omiros").greet()
+
+  // in Scala 3
+//  import scala.language.implicitConversions // required in Scala 3
+//  given string2PersonConversion: Conversion[String, Person] with {
+//    override def apply(x: String) = Person(x)
+//  }
 }

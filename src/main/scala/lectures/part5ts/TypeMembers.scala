a30760c060d4b621529d5626bd0e7e192e81dd6d
omirostrypatsas <115593155+omirostr1@users.noreply.github.com>
2025-06-27 22:49:07 +0100

    Section 6 - Type Members - Part 1

---
+++ src/main/scala/lectures/part5ts/TypeMembers.scala
@@ -0,0 +1,56 @@
+package lectures.part5ts
+
+import lectures.part5ts
+
+object TypeMembers extends App{
+
+  class Animal
+  class Dog extends Animal
+  class Cat extends Animal
+
+  class AnimalCollection {
+    type AnimalType // abstract class member
+    type BoundedAnimal <: Animal
+    type SuperBoundedAnimal >: Dog <: Animal
+    type AnimalC = Cat
+  } // abstract type members are mostly for us to help the compiler do some type inference for us
+
+  val ac = new AnimalCollection
+  val dog: ac.AnimalType = ???
+
+//  val cat: ac.BoundedAnimal = new Cat
+
+  val pup: ac.SuperBoundedAnimal = new Dog // this is allowed because SuperBoundedAnimal >: Dog but wouldn't work for Cat
+  val cat: ac.AnimalC = new Cat // as AnimalC = Cat
+
+  type CatAlias = Cat
+  val anotherCat: CatAlias = new Cat
+
+  // alternative to generics
+  trait MyList {
+    type T
+    def add(element: T): MyList
+  }
+
+  class NonEmptyList(value: Int) extends MyList {
+    override type T = Int // we can override an abstract type
+
+    override def add(element: Int): MyList = ???
+  }
+
+  // .type
+  type CatsType = cat.type
+  val newCat: CatsType = cat
+//  new CatsType // this will confuse the compiler and will not run
+
+  /*
+    Exercise - enforce a type to be applicable to SOME TYPES only
+   */
+  // LOCKED
+  trait MList {
+    type A
+    def head: A
+    def tail: MList
+  }
+
+}
